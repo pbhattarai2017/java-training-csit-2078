c8cd0969dbcec83094dc8a69dada7986ae184e04
Prakash Bhattarai <prakash1bhattarai25@gmail.com>
2024-04-17 10:03:33 +0545

    First day; basics of Java language system

---
+++ day01-20240416/MyFirstApp.java
@@ -0,0 +1,11 @@
+public class MyFirstApp {
+	public static void main(String[] args) {
+		for(int i = 1; i < 10; i ++) {
+			if(i == 5) {
+				System.out.println("At 5");
+			}
+			System.out.println("Hello World!");		
+		}
+	}
+}
+
---
+++ day01-20240416/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define ULLI unsigned long long int
+
+int main() {
+	printf("Hello world!");
+	return 0;
+}
