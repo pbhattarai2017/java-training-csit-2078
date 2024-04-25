91d5ac065ca5ce48b43478102060fad96d080dd9
Prakash Bhattarai <prakash1bhattarai25@gmail.com>
2024-04-25 11:16:05 +0545

    Problem solving intro using Hackerrank

---
+++ day07-20240425/Absolute.java
@@ -0,0 +1,15 @@
+public class Absolute {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	public static int abs(int num) {
+		if (num < 0) num = -num;
+		return num;
+	}
+	
+	abs(sum1-sum2);
+	
+}
+
